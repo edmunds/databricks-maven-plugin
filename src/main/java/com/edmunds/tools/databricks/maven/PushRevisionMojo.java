969621e24bc5fd16e0324852282d402194798958
Tim Huang <thuang@edmunds.com>
2022-01-27 17:06:31 -0800

    mojo for uploading revision to S3

---
+++ src/main/java/com/edmunds/tools/databricks/maven/PushRevisionMojo.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2018 Edmunds.com, Inc.
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
+package com.edmunds.tools.databricks.maven;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Uploads resource artifact to an S3 environment path that can be used as CodeDeploy revision.
+ */
+@Mojo(name = "push-revision")
+public class PushRevisionMojo extends UploadToS3Mojo {
+
+    /**
+     * The prefix to deploy to. This allows for jobs to have hardcoded s3 paths.
+     * Its especially useful for airflow dags.
+     * The default value is an example of how this could be structured.
+     */
+    @Parameter(name = "codeDeployRevisionKey", property = "code.deploy.revision.key",
+            defaultValue = "${project.groupId}/${project.artifactId}/${project.version}/${project.build.finalName}"
+                    + ".zip")
+    protected String codeDeployRevisionKey;
+
+    String createDeployAliasPath() throws MojoExecutionException {
+        validateRepoProperties();
+        String modifiedDatabricksRepoType = databricksRepoType + "://";
+        String modifiedDatabricksRepo = databricksRepo;
+        String modifiedDatabricksRepoKey = codeDeployRevisionKey;
+        if (databricksRepo.endsWith("/")) {
+            modifiedDatabricksRepo = databricksRepo.substring(0, databricksRepo.length() - 1);
+        }
+        if (codeDeployRevisionKey.startsWith("/")) {
+            modifiedDatabricksRepoKey = codeDeployRevisionKey.substring(1);
+        }
+        return String.format("%s%s/%s", modifiedDatabricksRepoType, modifiedDatabricksRepo, modifiedDatabricksRepoKey);
+    }
+}
